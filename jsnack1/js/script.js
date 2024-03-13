47e0c739a2cc8423c7f1c357c1ae21e44c719860
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-03-13 16:34:18 +0000

    add: jsnack1 and jsnack2

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,2 +1,15 @@
 // - L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
+
+// chiedere il numero all'utente
+const userNumber = parseInt(prompt("Inserire un numero"));
+
+// creare la variabile e stabilire se è pari o dispari e il relativo comportamento
+let userMessage;
+if (userNumber % 2 === 0) {
+  userMessage = userNumber;
+} else {
+  userMessage = userNumber + 1;
+}
+// risultato
+alert(userMessage);
--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,2 +1,13 @@
 // - L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// chiedere la prima e seconda parola
+const firstWord = prompt("Inserisci la prima parola");
+const secondWord = prompt("Inserisci la seconda parola");
+
+// stabilire la più corta e la più lunga per deciderne l'ordine
+if (firstWord.length > secondWord.length) {
+  alert(`${secondWord} ${firstWord}`);
+} else {
+  alert(`${firstWord} ${secondWord}`);
+}
