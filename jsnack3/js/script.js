832219a1dd18464704e86f10c9d8631ae9bc750c
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-03-13 16:46:24 +0000

    add: jsnack3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,3 +1,21 @@
 // - Crea due tag div con due id diversi:
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+const numbers = [42, 10, 31, 7, 8, 22, 53, 97];
+const redDiv = document.querySelector("#red");
+const greenDiv = document.querySelector("#green");
+
+// Scorriamo l'array
+for (let i = 0; i < numbers.length; i++) {
+  const thisNumber = numbers[i];
+
+  // Per ogni numero
+  // Se è pari lo mettiamo nel div verde
+  // altrimenti lo mettiamo nel div rosso
+  if (thisNumber % 2 === 0) {
+    greenDiv.innerHTML += `<div>${thisNumber}</div>`;
+  } else {
+    redDiv.innerHTML += `<div>${thisNumber}</div>`;
+  }
+}
